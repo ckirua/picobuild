ffd3df9bcd2029cfa664b1a52942d27b47d8280c
ckirua <gregorferrer@gmail.com>
2026-01-04 03:03:56 +0100

    patch cythonize

--- src/picobuild/__init__.py
+++ src/picobuild/__init__.py
@@ -1,12 +1,13 @@
 from .__about__ import __version__
 from ._setuptools import Extension, find_packages, setup
-from .cython_utils import get_cython_build_dir
+from .cython_utils import get_cython_build_dir, cythonize
 
 __all__: tuple[str, ...] = (
     # About
     "__version__",
     # CythonUtils
     "get_cython_build_dir",
+    "cythonize",
     # Setuptools
     "Extension",
     "find_packages",
--- tests/test_cython_utils.py
+++ tests/test_cython_utils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from picobuild import get_cython_build_dir
+from picobuild import get_cython_build_dir, Extension, cythonize
 
 
 class TestCythonUtils(unittest.TestCase):
@@ -8,3 +8,11 @@ class TestCythonUtils(unittest.TestCase):
         result = get_cython_build_dir()
         self.assertIsInstance(result, str)
         self.assertTrue(result.startswith("build/cython."))
+
+    def test_cythonize(self):
+        result = cythonize([Extension("test", ["test.pyx"])])
+        self.assertIsInstance(result, list)
+        self.assertTrue(len(result) > 0)
+        self.assertIsInstance(result[0], Extension)
+        self.assertEqual(result[0].name, "test")
+        self.assertEqual(result[0].sources, ["test.pyx"])
